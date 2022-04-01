8dc7925a2b6a09ddc1895841734b77fd42179206
Raphael Chauveau <raph.chauveau@gmail.com>
2022-04-01 22:14:32 +0200

    remove useless stuff

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 'use strict';
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-// import App from './App';
 
-// import {Ld49Game} from "./ld49/Ld49Game.js";
-import {BoidGame} from "./test_boids/BoidGame.js";
-// import * as serviceWorker from './serviceWorker';
+import {Ld49Game} from "./ld49/Ld49Game.js";
 
 console.log('APP');
 const canvas = document.getElementById('canvas');
 console.log('CANVAS', canvas);
-// const game = new Ld49Game(canvas);
-const game = new BoidGame(canvas);
+const game = new Ld49Game(canvas);
 console.log('GAME', game);
 game.run();
-
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
